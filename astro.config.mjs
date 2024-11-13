9c06852113ed5cf10be324f1ed8c922f151d65cb
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2024-11-13 20:29:23 +0530

    Ready for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,33 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mkcert from "vite-plugin-mkcert";
+import storyblok from "@storyblok/astro";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import { loadEnv } from "vite";
+
+const allEnv = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+const { STORYBLOK_ACESS_TOKEN } = allEnv;
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: STORYBLOK_ACESS_TOKEN,
+      bridge: true,
+      enableFallbackComponent: true,
+      livePreview: true,
+      components: {
+        // page: "storyblok/Page",
+        // feature: "storyblok/Feature",
+        // grid: "storyblok/Grid",
+        // teaser: "storyblok/Teaser",
+      },
+    }),
+    tailwind(),
+  ],
+  output: "server",
+  vite: {
+    plugins: [mkcert()],
+  },
+  adapter: vercel(),
+});
